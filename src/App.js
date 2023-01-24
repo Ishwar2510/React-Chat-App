a092506c0ad7b18a8ee652cfb3d9568930a579ba
Ishwar2510 <ishwar2510@gmail.com>
2023-01-25 00:21:53 +0530

    logout aded

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import {ChatEngine} from 'react-chat-engine'
+import ChatFeed from './components/ChatFeed'
+import LoginForm from './components/LoginForm';
 import './App.css';
 
+const projectID="be257d6b-0bd7-4822-9436-f9f3923603f2"
 function App() {
+  
+  if (!localStorage.getItem('username')) return <LoginForm />;
+  function logout(){
+    localStorage.removeItem('username')
+    localStorage.removeItem('password')
+    window.location.reload();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+    <button onClick={logout}style ={{cursor:"pointer" ,width :"75px",height:"50px",border:"2px solid ",borderRadius:"30px 30px 5px 30px",marginLeft:"10px", backgroundColor:"grey", position:"sticky" , bottom:"200px"}}>Logout</button>
+    </>
   );
 }
 
